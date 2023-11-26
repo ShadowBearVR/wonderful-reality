871557f399a7cb37ae6835190dade16ee66e9781
Collin MacDonald <cdm.helloworld@gmail.com>
2023-11-26 15:35:07 -0500

    Made ARCamera extension variables public

--- Sources/WonderfulReality/Extensions/ARCamera+Extension.swift
+++ Sources/WonderfulReality/Extensions/ARCamera+Extension.swift
@@ -8,24 +8,24 @@
 import ARKit
 
 extension ARCamera {
-    var aspectRatio: Float {
+    public var aspectRatio: Float {
         return projectionMatrix.m11 / projectionMatrix.m22
     }
     
-    var verticalFieldOfViewRadians: Float {
+    public var verticalFieldOfViewRadians: Float {
         let yScale = projectionMatrix.m22
         return 2 * atan(1 / yScale)
     }
     
-    var verticalFieldOfViewDegrees: Float {
+    public var verticalFieldOfViewDegrees: Float {
         return self.verticalFieldOfViewRadians * (180 / Float.pi)
     }
     
-    var horizontalFieldOfViewRadians: Float {
+    public var horizontalFieldOfViewRadians: Float {
         return 2 * atan(tan(verticalFieldOfViewRadians / 2) * aspectRatio)
     }
     
-    var horizontalFieldOfViewDegrees: Float {
+    public var horizontalFieldOfViewDegrees: Float {
         return self.horizontalFieldOfViewRadians * (180 / Float.pi)
     }
 }
